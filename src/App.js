459fe623d5aa6cd5ebdf2438ef96cd13e84fa988
colchang <colchang@iu.edu>
2020-06-17 17:45:17 -0700

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">NYT Google Books Search </h1>
+          <h1 className="App-title">Google Books Search</h1>
         </header>
         <div className="container main-content">
           <FormGroup>
